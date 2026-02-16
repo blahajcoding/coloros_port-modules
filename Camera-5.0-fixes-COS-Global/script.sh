25e89659d22b6138cbb03e4fbeed059e1993fa7a
blahajcoding <venusisonvenus@gmail.com>
2026-02-16 21:20:42 +0000

    Update: Fix typos in Camera-5.0-fixes* modules Changes cp to cp -rf

--- Camera-5.0-fixes-COS-Global/script.sh
+++ Camera-5.0-fixes-COS-Global/script.sh
@@ -4,4 +4,4 @@ rm -rf build/portrom/images/my_product/app/OplusCamera
 rm -rf build/portrom/images/my_product/product_overlay/framework/com.oplus.camera.*.jar
 echo "ro.vendor.oplus.camera.isSupportLumo=1" >> build/portrom/images/my_product/etc/bruce/build.prop
 unzip -o devices/${base_product_device}/camera5.0-fix_cos_global.zip -d build/portrom/images/
-cp $module_files/* build/portrom/images/
+cp -rf $module_files/* build/portrom/images/
--- Camera-5.0-fixes-ODM/script.sh
+++ Camera-5.0-fixes-ODM/script.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-cp $module_files/* build/portrom/images/
+cp -rf $module_files/* build/portrom/images/
